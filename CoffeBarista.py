432baf3947dac0777b299c4df6ec31b95ff4f5b6
mjpy2222 <mindyjalbers@gmail.com>
2024-05-15 21:43:39 -0500

    initial draft

---
+++ CoffeBarista.py
@@ -0,0 +1,13 @@
+#Let's start a coffee shop together!!
+#Let's build robot Barista!!
+
+print("Hello, Welcome to NetworkChuck Coffee!!!!!")
+name = input("What is your name?\n")
+
+print("Hello " + name + ", thank you so much for coming in today.\n")
+
+menu = "Black Coffee, Espresso, Latte, Cappucino"
+
+order = input(name + ", what would you like from our menu today? Here is what we are serving:\n" + menu + "\n")
+
+print("Sounds good " + name + ", we will have that " + order + " ready for you in a moment.")
